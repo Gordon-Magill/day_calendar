7ab2ddd5525a6ce5708afbf9889070789ee31168
Gordon-Magill <gordon.magill@gmail.com>
2022-08-28 18:07:05 -0700

    Misc. JS cleanup + improved styling for text contrast

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# day_calendar
+# Day calendar
+
+A basic calendar app to help plan out busy work days.
+Time blocks are color coded by their status, and each row has a text area that the user can
+fill in event details for. The save button allows you to lock in events and come back to the
+page later to check in on your schedule again. Hour block coloring refreshes over time as well.
+
+ Link to deployed page:
+ https://gordon-magill.github.io/code_quiz/
+ ![Deployed page](./assets/images/deployed_screenshot.png)
+## Authors
+
+- [@Gordon-Magill](https://github.com/Gordon-Magill/)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,11 @@ setInterval(function (){
     $("#currentDay").text(currentTime)
 },60000)
 
-
-
 // Create a single row of the calendar
 function addCalendarRow(timeSlotMoment) {
 
     // Create a unique ID for the HTML elements that correspond to this hour block
     var hourID = 'timeSlot'+timeSlotMoment.hour();
-    // console.log(hourID);
 
     // Create the row container
     var timeSlotRow = $('<section>');
@@ -33,39 +30,38 @@ function addCalendarRow(timeSlotMoment) {
     timeSlotTextContainer.text(localStorage.getItem(hourID) || '')
     timeSlotTextContainer.addClass('col-8');
 
-
     // Save text for the calendar event to localStorage
     function setCalDetails(event) {
         event.preventDefault();
         localStorage.setItem(hourID,timeSlotTextContainer.val());
-
     }
 
     // Create the save section of the row
     var timeSlotSaveButton = $('<button>');
-    timeSlotSaveButton.text('💾')
+    timeSlotSaveButton.text('💾');
     timeSlotSaveButton.addClass('col-2 saveBtn');
     timeSlotSaveButton.on('click', setCalDetails);
-    // timeSlotSaveButton.attr('src','./assets/images/floppy-icon.png')
 
-    // // Create the actual button for saving content
-    // var saveButton = $('<button>')
 
     // Function to format elements based on time relative to current time
+    // Removes all time-based classes and adds back the relevant one based on the current time
     function updateTimeColoring() {
-        if (timeSlotMoment.isBefore(moment().minute(0).second(0))) {
+
+        if (timeSlotMoment.hour() < moment().hour()) {
             // If the time slot is in the past...
             timeSlotTextContainer.removeClass('past');
             timeSlotTextContainer.removeClass('present');
             timeSlotTextContainer.removeClass('future');
             timeSlotTextContainer.addClass('past');
+            console.log('Set past')
     
-        } else if (timeSlotMoment.isAfter(moment().minute(0).second(0))) {
+        } else if (timeSlotMoment.hour() > moment().hour()) {
             // If the time slot is in the future...
             timeSlotTextContainer.removeClass('past');
             timeSlotTextContainer.removeClass('present');
             timeSlotTextContainer.removeClass('future');
             timeSlotTextContainer.addClass('future');
+            console.log('Set future')
     
         } else {
             // If time slot is not in the past or future, it must be the present!
@@ -73,8 +69,9 @@ function addCalendarRow(timeSlotMoment) {
             timeSlotTextContainer.removeClass('present');
             timeSlotTextContainer.removeClass('future');
             timeSlotTextContainer.addClass('present');
+            console.log('Set present')
         };
-        console.log('Updated time slot coloring at '+moment().format(headerDateFormat))
+        // console.log('Updated time slot coloring at '+moment().format(headerDateFormat))
     }
 
     // Update the coloring immediately and continue comparisons every minute
@@ -95,14 +92,3 @@ for (var i=7;i<19;i++) {
     // Create times from 6am - 7pm
     addCalendarRow(moment().hour(i).minute(0).second(0))
 };
-
-
-
-
-
-
-// var newTimeSlot = $('<section>');
-// newTimeSlot
-
-
-// Create replicate time slots
